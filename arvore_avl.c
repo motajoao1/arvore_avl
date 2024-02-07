a8bc6177ad3fde8cab54ea565a96a14e7af87181
João Paulo Mota <99102663+motajoao1@users.noreply.github.com>
2024-02-07 13:37:46 -0300

    Add files via upload

---
+++ arvore_avl.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("\t             *\n");
+  printf("\t            ***\n");  
+  printf("\t           *****\n");
+  printf("\t          *******\n");
+  printf("\t         *********\n");
+  printf("\t        ***********\n");
+  printf("\t       *************\n");
+  printf("           *ARVORE  AVL*     \n");
+  printf("\t      ***************\n");
+  printf("\t     *****************\n");
+  printf("\t    *******************\n");
+  printf("\t   *********************\n");
+  printf("\t  ***********************\n");
+  printf("\t *************************\n");
+  printf("\t***************************\n");
+  printf("\t            ***\n");
+  }
